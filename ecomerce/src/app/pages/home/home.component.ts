2b3604a2a02dbf09b65c38e276576deabeb8a145
Andre Costa Silva <andreshippuden3711@gmail.com>
2022-12-20 11:41:06 -0300

    feat - create services and menu

--- ecomerce/src/app/pages/home/home.component.ts
+++ ecomerce/src/app/pages/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderDetailsService } from 'src/app/services/order-details.service';
-OrderDetailsService
 
 @Component({
   selector: 'app-home',
--- ecomerce/src/app/pages/menu/menu.component.ts
+++ ecomerce/src/app/pages/menu/menu.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+
+  constructor(private service: OrderDetailsService){}
+
+  data:any;
+
+  ngOnInit(): void {
+    this.data = this.service.details
+  }
 
 }
