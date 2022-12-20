b0b9028a276a32bd50e2e2030e7cb0c87a131258
Andre Costa Silva <andreshippuden3711@gmail.com>
2022-12-20 12:46:41 -0300

    feat - create and impelmets menuPages

--- ecomerce/src/app/app.module.ts
+++ ecomerce/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MenuComponent } from './pages/menu/menu.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { MenupagesComponent } from './pages/menupages/menupages.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ContactComponent } from './pages/contact/contact.component';
     MenuComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    MenupagesComponent
   ],
   imports: [
     BrowserModule,
