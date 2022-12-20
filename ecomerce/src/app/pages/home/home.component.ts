a216c6ce74a9a2624502c1a6ff6dece13993c2c4
Andre Costa Silva <andreshippuden3711@gmail.com>
2022-12-20 11:28:46 -0300

    feat - create layout homePage

--- ecomerce/src/app/pages/home/home.component.ts
+++ ecomerce/src/app/pages/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
+OrderDetailsService
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  constructor(private service: OrderDetailsService){}
+
+  data:any;
+
+  ngOnInit(): void {
+    this.data = this.service.details
+  }
 
 }
---
+++ ecomerce/src/app/services/order-details.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderDetailsService {
+
+  constructor() { }
+
+  details = [
+    {
+      id:1,
+      name:"Roupa Infantil Blogueirinha",
+      price:200,
+      img:"https://cf.shopee.com.br/file/e2c0fd72a0dc2c7b7e4ac7fa90a3900b"
+    },
+    {
+      id:2,
+      name:"Roupa Infantil Blogueirinha",
+      price:200,
+      img:"https://cf.shopee.com.br/file/e2c0fd72a0dc2c7b7e4ac7fa90a3900b"
+    },
+    {
+      id:3,
+      name:"Roupa Infantil Blogueirinha",
+      price:200,
+      img:"https://cf.shopee.com.br/file/e2c0fd72a0dc2c7b7e4ac7fa90a3900b"
+    },
+    {
+      id:4,
+      name:"Roupa Infantil Blogueirinha",
+      price:200,
+      img:"https://cf.shopee.com.br/file/e2c0fd72a0dc2c7b7e4ac7fa90a3900b"
+    },
+    {
+      id:5,
+      name:"Roupa Infantil Blogueirinha",
+      price:200,
+      img:"https://cf.shopee.com.br/file/e2c0fd72a0dc2c7b7e4ac7fa90a3900b"
+    },
+    {
+      id:6,
+      name:"Roupa Infantil Blogueirinha",
+      price:200,
+      img:"https://cf.shopee.com.br/file/e2c0fd72a0dc2c7b7e4ac7fa90a3900b"
+    },
+    {
+      id:7,
+      name:"Roupa Infantil Blogueirinha",
+      price:200,
+      img:"https://cf.shopee.com.br/file/e2c0fd72a0dc2c7b7e4ac7fa90a3900b"
+    },
+    {
+      id:8,
+      name:"Roupa Infantil Blogueirinha",
+      price:200,
+      img:"https://cf.shopee.com.br/file/e2c0fd72a0dc2c7b7e4ac7fa90a3900b"
+    }
+  ]
+}
